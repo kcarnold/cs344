c8c8a741f959ea2df904619d9168526e25f926ba
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2023-01-19 08:53:14 -0500

    Allow nb_strip to run with old nbformat

--- nb_strip.py
+++ nb_strip.py
@@ -6,9 +6,11 @@ in_filename = sys.argv[1]
 out_fname = sys.argv[2]
 
 nb = nbformat.read(in_filename, as_version=nbformat.NO_CONVERT)
-changes, nb = nbformat.validator.normalize(nb)
-if changes:
-    print("Fixed", changes, "validation issues")
+if hasattr(nbformat.validator, 'normalize'):
+    # Old nbformat didn't have this.
+    changes, nb = nbformat.validator.normalize(nb)
+    if changes:
+        print("Fixed", changes, "validation issues")
 
 # Reset the kernel spec because sometimes my local environment has a different ipykernel name.
 nb['metadata']['kernelspec'] = {'display_name': 'Python 3', 'language': 'python', 'name': 'python3'}
